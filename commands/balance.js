b7ca0a9e62f7ea82000b1f1cb3f54a3cd9c3da2b
Ionut Moraru <morion4000@gmail.com>
2020-10-19 14:00:43 +0300

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -41,13 +41,24 @@ var Command = function(bot) {
       })
           .then(function (found_user) {
             if (found_user) {
-              resp = 'Balance @' + found_user.telegram_username + ': ' + numeral(found_user.balance).format('0,0') + ' WEBD. You can add more funds using /topup';
+              var yearly_percentage = 0;
+
+              if (found_user.balance >= config.staking.tier3_threshold) {
+                yearly_percentage = config.staking.yearly_percentage_tier3;
+              } else if (found_user.balance >= config.staking.tier2_threshold) {
+                yearly_percentage = config.staking.yearly_percentage_tier2;
+              } else if (found_user.balance >= config.staking.tier1_threshold) {
+                yearly_percentage = config.staking.yearly_percentage_tier1;
+              }
+
+              resp = '💰 Balance: *' + numeral(found_user.balance).format('0,0') + '* WEBD. Receiving /staking rewards @ *' + yearly_percentage + '%* per year.\n\n';
+              resp += '💵 You can add more funds using /topup.';
             } else {
               resp = 'Your user can not be found. Create a new acount /start';
             }
 
             bot.sendMessage(msg.chat.id, resp, {
-              //parse_mode: 'Markdown',
+              parse_mode: 'Markdown',
               disable_web_page_preview: true,
               disable_notification: true,
             });
