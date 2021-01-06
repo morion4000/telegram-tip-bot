4af6d58d3f88a5bbf66b0bb79bc7e952df20ef38
Ionut Moraru <morion4000@gmail.com>
2021-01-06 14:14:28 +0200

    update

--- workers/process_withdraws.js
+++ workers/process_withdraws.js
@@ -1,5 +1,6 @@
 const user_model = require('./../models').user.model;
 const transaction_model = require('./../models').transaction.model;
+const coin_model = require('./../models').coin.model;
 const config = require('./../config');
 const TelegramBot = require('node-telegram-bot-api');
 const numeral = require('numeral');
@@ -19,6 +20,16 @@ exports.handler = async function (event) {
   const bot = new TelegramBot(config.telegram.token, {
     polling: false,
   });
+
+  const webdollar = await coin_model.findOne({
+    where: {
+      ticker: 'WEBD',
+    },
+  });
+
+  if (!webdollar) {
+    throw new Error('Coin not found');
+  }
 
   const transactions = await transaction_model.findAll({
     where: {
@@ -94,18 +105,23 @@ exports.handler = async function (event) {
       });
 
       if (user.telegram_id) {
+        const amount_usd = parseFloat(transaction.amount * webdollar.price_usd);
         const resp =
           '🆕 *Update*: Withdraw transaction is being processed. Your wallet `' +
           transaction.transaction_to +
           '` will receive *' +
           numeral(transaction.amount).format('0,0') +
-          ' WEBD*.';
+          '* WEBD ($' + numeral(amount_usd).format('0,0') + ').';
 
-        await bot.sendMessage(user.telegram_id, resp, {
-          parse_mode: 'Markdown',
-          disable_web_page_preview: true,
-          disable_notification: true,
-        });
+        bot
+          .sendMessage(user.telegram_id, resp, {
+            parse_mode: 'Markdown',
+            disable_web_page_preview: true,
+            disable_notification: true,
+          })
+          .catch((error) => {
+            console.log(error.message);
+          });
       }
     } else {
       console.error('error sending transaction', error, body);
