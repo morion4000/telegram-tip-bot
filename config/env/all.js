dcf4da839753782fcf8ebc5f0998f16320a96538
Ionut Moraru <morion4000@gmail.com>
2021-04-23 10:44:25 +0300

    Update all.js

--- config/env/all.js
+++ config/env/all.js
@@ -46,7 +46,7 @@ module.exports = {
     tier2_threshold: 1000000,
     tier3_threshold: 10000000,
   },
-  public_channels: [-1001310583642],
+  public_channels: [-1001310583642, -1001329381051],
   mailgun: {
     key: process.env.MAILGUN_KEY,
     domain: 'mg.hostero.eu',
