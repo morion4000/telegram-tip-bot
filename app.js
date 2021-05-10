886c71cc522539925161748968dce3365883062e
Ionut Moraru <morion4000@gmail.com>
2021-05-10 12:32:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-s;
+
 const webhooks = require('./controllers/webhooks');
 const config = require('./config');
 
