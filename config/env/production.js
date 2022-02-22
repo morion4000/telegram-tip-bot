efbb3c8cdb610715d8c0d758377fdc8270ce725d
Ionut Moraru <morion4000@gmail.com>
2022-02-22 11:36:25 +0200

    u

--- config/env/production.js
+++ config/env/production.js
@@ -27,8 +27,8 @@ module.exports = {
   game: {
     id: 'hauntedtower',
     url: 'https://telegram.hauntedtower.com',
-    max_score: 150,
-    max_user_rewards: 1300,
+    max_score: 200,
+    max_user_rewards: 5000,
     telegram_channel: -1001510982248,
     telegram_origin: 'https://telegram.hauntedtower.com',
   },
