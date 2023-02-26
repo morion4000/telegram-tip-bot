9f526b6f8ebc12ad2ce7551ca17b91876ab98fc3
Ionut Moraru <morion4000@gmail.com>
2023-02-26 09:02:43 +0200

    Update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -26,8 +26,8 @@ module.exports = {
   game: {
     id: 'hauntedtower',
     url: 'https://a0e3-79-119-254-64.ngrok.io',
-    max_score: 1000,
-    max_user_rewards: 10000,
+    max_score: 10000,
+    max_user_rewards: 100000,
     telegram_channel: 528354447,
     telegram_origin: 'http://localhost:4200',
     scores_key: process.env.SCORES_KEY,
